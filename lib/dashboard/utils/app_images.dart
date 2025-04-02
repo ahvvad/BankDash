1f0a1ede6fd7b8c5bd8492ddf34ac6815eacac3e
ahvvad <ahvadd.k@gmail.com>
2025-04-02 10:40:04 +0200

    Add required packages
    - google_fonts, fl_chart, flutter_svg, device_preview
    - add images class

---
+++ lib/dashboard/utils/app_images.dart
@@ -0,0 +1,60 @@
+class AppImages {
+  //? Icons assets
+  // chipCard = chip_card.svg
+  static const String chipCard = 'assets/icons/chip_card.svg';
+
+  // creditCard = credit_card.svg
+  static const String creditCard = 'assets/icons/credit_card.svg';
+
+  // dollar = dollar.svg
+  static const String dollar = 'assets/icons/dollar.svg';
+
+  // econometrics = econometrics.svg
+  static const String econometrics = 'assets/icons/econometrics.svg';
+
+  // investment = economic_investment.svg
+  static const String investment = 'assets/icons/investment.svg';
+
+  // home = home.svg
+  static const String home = 'assets/icons/home.svg';
+
+  // loan = loan.svg
+  static const String loan = 'assets/icons/loan.svg';
+
+  // magnifyingGlass = magnifying_glass.svg
+  static const String search = 'assets/icons/search.svg';
+
+  // masterCard = master_card.svg
+  static const String masterCard = 'assets/icons/master_card.svg';
+
+  // notification = notification.svg
+  static const String notification = 'assets/icons/notification.svg';
+
+  // paypal = paypal.svg
+  static const String paypal = 'assets/icons/paypal.svg';
+
+  // send = send.svg
+  static const String send = 'assets/icons/send.svg';
+
+  // service = service.svg
+  static const String service = 'assets/icons/service.svg';
+
+  // settingsSolid = settings_solid.svg
+  static const String settingsSolid = 'assets/icons/settings_solid.svg';
+
+  // settings = settings.svg
+  static const String settings = 'assets/icons/settings.svg';
+
+  // transfer = transfer.svg
+  static const String transfer = 'assets/icons/transfer.svg';
+
+  // user = user.svg
+  static const String user = 'assets/icons/user.svg';
+
+  // wallet = wallet.svg
+  static const String wallet = 'assets/icons/wallet.svg';
+
+  //? Icons assets
+  // logo = Logo.svg
+  static const String logo = 'assets/images/Logo.svg';
+}
