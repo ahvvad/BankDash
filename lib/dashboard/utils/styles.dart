9d3ba0acab14bf4bd649f7a784a90e8727efced7
ahvvad <ahvadd.k@gmail.com>
2025-04-11 14:11:35 +0200

    Add app theming

---
+++ lib/dashboard/utils/styles.dart
@@ -0,0 +1,48 @@
+import 'package:bankdash/dashboard/utils/font_weight_helper.dart';
+import 'package:flutter/material.dart';
+
+class TextStyles {
+  // SemiBold Font
+  static TextStyle font24SemiBold = const TextStyle(
+    fontWeight: FontWeightHelper.semiBold,
+    fontSize: 28,
+  );
+  static TextStyle font22SemiBold = const TextStyle(
+    fontWeight: FontWeightHelper.semiBold,
+    fontSize: 22,
+  );
+  static TextStyle font20SemiBold = const TextStyle(
+    fontWeight: FontWeightHelper.semiBold,
+    fontSize: 20,
+  );
+
+  // Medium Font
+  static TextStyle font18Medium = const TextStyle(
+    fontWeight: FontWeightHelper.medium,
+    fontSize: 18,
+  );
+  static TextStyle font16Medium = const TextStyle(
+    fontWeight: FontWeightHelper.medium,
+    fontSize: 16,
+  );
+  static TextStyle font15Medium = const TextStyle(
+    fontWeight: FontWeightHelper.medium,
+    fontSize: 15,
+  );
+
+  // Regular Font
+  static TextStyle font16Regular = const TextStyle(
+    fontWeight: FontWeightHelper.regular,
+    fontSize: 16,
+  );
+  static TextStyle font15Regular = const TextStyle(
+    fontWeight: FontWeightHelper.regular,
+    fontSize: 15,
+  );
+
+  // Light Font
+  static TextStyle font13light = const TextStyle(
+    fontWeight: FontWeightHelper.light,
+    fontSize: 13,
+  );
+}
