9ece92f647a13ae35b0ceb59d65e756128b953bb
ahvvad <ahvadd.k@gmail.com>
2025-04-11 14:20:41 +0200

    Add platforms layouts

--- lib/dashboard/views/dashboard_view.dart
+++ lib/dashboard/views/dashboard_view.dart
@@ -1,5 +1,7 @@
 import 'package:bankdash/dashboard/widgets/adaptive_layout_widget.dart';
 import 'package:bankdash/dashboard/widgets/dashboard_desktop_layout.dart';
+import 'package:bankdash/dashboard/widgets/dashboard_mobile_layout.dart';
+import 'package:bankdash/dashboard/widgets/dashboard_tablet_layout.dart';
 import 'package:flutter/material.dart';
 
 class DashboardView extends StatelessWidget {
@@ -9,8 +11,8 @@ class DashboardView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: AdaptiveLayout(
-        mobileLayout: (context) => const SizedBox(),
-        tabletLayout: (context) => const SizedBox(),
+        mobileLayout: (context) => const DashboardMobileLayout(),
+        tabletLayout: (context) => const DashboardTabletLayout(),
         desktopLayout: (context) => const DashboardDesktopLayout(),
       ),
     );
---
+++ lib/dashboard/widgets/dashboard_tablet_layout.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DashboardTabletLayout extends StatelessWidget {
+  const DashboardTabletLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('DashboardTabletLayout'),
+    );
+  }
+}
